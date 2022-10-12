5fd3241f696202bebd4e5ff4b351f07a9b63b36b
TSwearingen <tswearingen06@gmail.com>
2022-10-12 16:15:36 -0500

    first commit to push changes

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+document.getElementById('username').hidden = true;
+document.getElementById('username').hidden = true;
+
+let showUsername = function() {
+        document.getElementById("username").hidden = false;
+};
+
+document.getElementById('LinkedIn').onclick = showUsername;
